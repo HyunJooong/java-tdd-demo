7da5d6f768563aabd0ccbe1cd957cb64e7c6b68a
chooHyun <hjchoo19950327@gmail.com>
2025-10-23 00:37:01 +0900

    [mission1] Refactor: 특정 유저 포인트 내역 조회 Red 테스트 제거

--- src/test/java/io/hhplus/tdd/service/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/service/PointServiceTest.java
@@ -45,20 +45,6 @@ class PointServiceTest {
     }
 
     @Test
-    @DisplayName("히스토리가 없는 유저의 포인트 내역 조회 시 예외가 발생한다 - RED")
-    void getPointHistory_noHistory_throwsException() {
-        // given: 히스토리가 전혀 없는 유저 ID
-        long userId = 999L;
-
-        // when: 포인트 내역 조회
-        List<PointHistory> result = pointService.getPointHistory(userId);
-
-        // then: 빈 리스트가 아닌 예외가 발생해야 함
-        // 현재 구현은 빈 리스트를 반환하므로 이 테스트는 실패함 (RED)
-        assertThat(result).isNotEmpty();
-    }
-
-    @Test
     @DisplayName("특정 유저의 여러 포인트 내역을 조회한다 - GREEN & Refactor")
     void getPointHistory_withMultipleHistories_success() {
         // given: 유저 1L의 여러 포인트 내역이 저장되어 있음
